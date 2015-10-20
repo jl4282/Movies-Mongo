c047c8fe7f1f6b6ad099b0139b64330b537872db
Jesse Lifshitz <Jesse.Lifshitz@sesame.org>
2015-10-20 11:57:16 -0400

    show all movies (/movies) route)

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ var express = require('express');
 var app = express();
 var handlebars = require('express-handlebars').create({'defaultLayout':'main'});
 
+require( './db' );
+
+var mongoose = require('mongoose');
+var Movie = mongoose.model('Movie');
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
-
+app.get('/movies', function(req, res){
+  Movie.find({}, function(err, movies, count){
+    res.render('movies', {movies: movies});
+  });
+});
 
 app.listen(3000);
 console.log('Server started on port 3000\n');
